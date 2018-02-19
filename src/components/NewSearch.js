c745621dfc64b8e991715176ecfe4bd289e6e173
Stuart Harper <stuartharper044@gmail.com>
2018-02-19 11:35:36 -0700

    added search terms

--- src/components/NewSearch.js
+++ src/components/NewSearch.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getUserInfo } from '../ducks/reducer';
+import Header from './Header';
+import SearchTerms from './SearchTerms';
+import axios from 'axios';
 
 class NewSearch extends Component {
   constructor( props ) {
@@ -9,20 +12,88 @@ class NewSearch extends Component {
     this.state = {
       workingLocation: "",
       workingAmmount: 0,
-      workingCashDealTog: true,
+      workingCashDealTog: 0,
       workingRate: 0,
-      workingMoveInTog: true,
-      workingSortBy: 1,
-      workingEmailResults: true
+      workingMoveInTog: 0,
+      workingSortBy: 0,
+      workingEmailResults: 0
     };
   
-    // this.saveSearch=this.saveSearch.bind(this);
-    // this.handleChange=this.handleChange.bind(this);
+    this.saveSearch=this.saveSearch.bind(this);
+    this.handleChange=this.handleChange.bind(this);
+    this.cancel=this.cancel.bind(this);
+    this.run=this.run.bind(this);
   }
+
+  handleChange( name, value ) {
+    if( name === 'workingMoveInTog' || name === 'workingCashDealTog' || name === 'workingEmailResults' ) {
+      if ( this.state[name] === 0 ){
+        this.setState({
+          [ name ]: 1
+        })
+      }
+      else {
+        this.setState({
+          [ name ]: 0
+        })
+      }
+    }
+    else{
+      this.setState({
+        [ name ]: value
+      })
+    }
+  }
+
+  saveSearch(){
+    //axios.post('/save_search', {
+    console.log({
+      location: this.state.workingLocation,
+      amount: this.state.workingAmmount,
+      cashDeal: this.state.workingCashDealTog,
+      rate: this.state.workingRate,
+      moveIn: this.state.workingMoveInTog,
+      sortBy: this.state.workingSortBy,
+      emailResults: this.state.workingEmailResults
+    })
+  }
+
+  cancel(){
+    this.setState({
+      workingLocation: "",
+      workingAmmount: 0,
+      workingCashDealTog: 0,
+      workingRate: 0,
+      workingMoveInTog: 0,
+      workingSortBy: 0,
+      workingEmailResults: 0
+    })
+  }
+
+  run(){
+    axios.put('/search', {
+      location: this.state.workingLocation,
+      amount: this.state.workingAmmount,
+      cashDeal: this.state.workingCashDealTog,
+      rate: this.state.workingRate,
+      moveIn: this.state.workingMoveInTog,
+      sortBy: this.state.workingSortBy,
+      emailResults: this.state.workingEmailResults
+    })//add a .then that calls redux with the result and then pushes to the results page. perhaps a loading animation on the button would work well.
+  }
+
   render(){
     return(
-      <div>
-        NewSearch Component
+      <div className='NewSearch'>
+        <Header location='New Search'/>
+        <section className='termsHolder'>
+          <SearchTerms handleChange={this.handleChange} state={this.state}/>
+        </section>
+        <div className='buttonHolder'>
+          <button className='searchButton' onClick={ () => this.saveSearch() }>Save</button>
+          <button className='searchButton' onClick={ () => this.run() }>Search</button>
+          <button className='searchButton' onClick={ () => this.cancel() }>Cancel</button>
+        </div>
       </div>
     )
   }
--- src/components/SearchTerms.js
+++ src/components/SearchTerms.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react'
 
 class SearchTerms extends Component {
-  
   render(){
     return(
-      <div>
-        SearchTerms Component
+      <div className='SearchTerms'>
+        <label htmlFor='workingLocation'>City or Zip Code</label>
+        <input type='text' id='workingLocation' value={this.props.state.workingLocation} onChange={ (event) => this.props.handleChange( event.target.id, event.target.value )}/>
+        
+        <label htmlFor='workingAmmount'>Cash Ammount for Purchase/Downpayment</label>
+        <input type='text' id='workingAmmount' value={this.props.state.workingAmmount} onChange={ (event) => this.props.handleChange( event.target.id, event.target.value )}/>
+        
+        <label htmlFor='workingCashDealTog'>Are you purchasing the property in a cash deal?</label>
+        <div className='switch'>
+          <input type='checkbox' id='workingCashDealTog' checked={this.props.state.workingCashDealTog} onChange={ (event) => this.props.handleChange( event.target.id, event.target.value )}/>
+          <span className="slider round"></span>
+        </div>
+
+        <label htmlFor='workingRate'>What is your anticipated inerest rate? (Anual percent)</label>
+        <input type='text' id='workingRate' value={this.props.state.workingRate} onChange={ (event) => this.props.handleChange( event.target.id, event.target.value )}/>
+        
+        <label htmlFor='workingMoveInTog'>Will you be moving into the home for a time?</label>
+        <div className='switch'>
+          <input type='checkbox' id='workingMoveInTog' checked={this.props.state.workingMoveInTog} onChange={ (event) => this.props.handleChange( event.target.id, event.target.value )}/>
+          <span className="slider round"></span>
+        </div>
+
+        <div>
+          Sort results by:
+          <label htmlFor='SortBy1'>Cap Rate 
+          <input type='radio' name='workingSortBy' value={0} onChange={ (event) => this.props.handleChange( event.target.name, event.target.value )} checked="checked"/>
+          </label>
+          <label htmlFor='SortBy1'>Cash Yield 
+          <input type='radio' name='workingSortBy' value={1} onChange={ (event) => this.props.handleChange( event.target.name, event.target.value )}/>
+          </label>
+          <label htmlFor='SortBy1'>Cash Flow 
+          <input type='radio' name='workingSortBy' value={2} onChange={ (event) => this.props.handleChange( event.target.name, event.target.value )} />
+          </label>
+        </div>
+        
+        <label htmlFor='workingEmailResults'>Do you want an email sent of the results?</label>
+        <div className='switch'>
+          <input type='checkbox' id='workingEmailResults' checked={this.props.state.workingEmailResults} onChange={ (event) => this.props.handleChange( event.target.id, event.target.value ) }/>
+          <span className="slider round"></span>
+        </div>
+
       </div>
     )
   }
